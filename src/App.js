0f85bf98d91ccd819ae43c524f35e5054f090a18
mthaissouza <maria.tha2002@gmail.com>
2023-02-13 10:07:50 -0300

    feat: Criação do Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
 
 const App = () => {
     return (
         <>
-            <div>App</div>
+            <Header></Header>
             <GlobalStyle />
         </>
     )
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    Container,
+    Headers,
+    Title
+} from "./styles"
+
+const Header = () => {
+    return (
+        <Container>
+            <Headers>
+                <Title>
+                    Controle Financeiro
+                </Title>
+            </Headers>
+        </Container>
+    )
+}
+
+export default Header
